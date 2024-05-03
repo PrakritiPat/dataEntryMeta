a41b3d740a370c9810d5cc2f7a06f1ace53d4b86
SCU Imaginarium (Jessie) <>
2024-05-03 12:51:12 -0700

    we need raycast next

--- Assets/scripts/datacollect.cs
+++ Assets/scripts/datacollect.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -92,10 +92,8 @@ public class datacollect : MonoBehaviour
             outStream.Close();
         }
     }
-}
+}*/
 
-
-/* original code izzy changed 4/29
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -105,88 +103,91 @@ using System;
 
 public class datacollect : MonoBehaviour
 {
-
-    [SerializeField] List<string[]> rowData = new List<string[]>();
-    [SerializeField] Transform objectTransform; // Reference to the object's transform
-    [SerializeField] List<Transform> transformList;
+    string filePath; // Path to the CSV file
+    StreamWriter outStream; // Stream writer to write to the file
 
     // Use this for initialization
     void Start()
     {
-        //fill list of all transforms we're tracking
-
-        // Creating First row of titles manually..
-        //loop through the list of transforms to see how many inputs we need (position xyz, rotation xyz)
-
-        string[] rowDataTemp = new string[3]; // Modified to hold XYZ coordinates
-        rowDataTemp[0] = "X";
-        rowDataTemp[1] = "Y";
-        rowDataTemp[2] = "Z";
-        rowData.Add(rowDataTemp);
-
-    }
-
-    private void Update()
-    {
-        Save();
-    }
-
-    void Save()
-    {
-        string filePath = getPath();
+        // Get the file path
+        filePath = getPath();
 
         // Check if file already exists
-        if (File.Exists(filePath))
+        if (!File.Exists(filePath))
         {
-            // If file exists, do nothing and exit the method
-            return;
+            // If file doesn't exist, create a new one
+            CreateNewFile();
+        }
+        else
+        {
+            // If file exists, open it in append mode
+            outStream = System.IO.File.AppendText(filePath);
         }
 
-        string[] rowDataTemp = new string[3];
-        // Collect and save the XYZ coordinates
-        rowDataTemp = new string[3];
-        for (int i = 0; i < transformList.Count; i++)
-        {
-            Vector3 position = objectTransform.position; // Get object's position
-            rowDataTemp[0] = position.x.ToString(); // X coordinate
-            rowDataTemp[1] = position.y.ToString(); // Y coordinate
-            rowDataTemp[2] = position.z.ToString(); // Z coordinate
-            rowData.Add(rowDataTemp);
-        }
+        // Write the initial row for object name, position X, position Y, position Z, rotation X, rotation Y, rotation Z, and timestamp
+        outStream.WriteLine("Object Name,Position X,Position Y,Position Z,Rotation X,Rotation Y,Rotation Z,Timestamp");
 
-        string[][] output = new string[rowData.Count][];
-
-        for (int i = 0; i < output.Length; i++)
-        {
-            output[i] = rowData[i];
-            Debug.Log(rowData[i]);
-        }
-
-        int length = output.GetLength(0);
-        string delimiter = ",";
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int index = 0; index < length; index++)
-            sb.AppendLine(string.Join(delimiter, output[index]));
-
-        // Write data to file
-        StreamWriter outStream = System.IO.File.CreateText(filePath);
-        outStream.WriteLine(sb);
-        outStream.Close();
+        // Start tracking position and rotation changes
+        StartCoroutine(TrackPositionAndRotation());
     }
+
+    IEnumerator TrackPositionAndRotation()
+    {
+        // Track positions and rotations indefinitely
+        while (true)
+        {
+            // Get all GameObjects in the scene
+            GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+            // Iterate over each object
+            foreach (GameObject obj in allObjects)
+            {
+                // Get the object's name, position, and rotation
+                string objectName = obj.name;
+                Vector3 position = obj.transform.position;
+                Vector3 rotation = obj.transform.eulerAngles;
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"); // Get current timestamp
+
+                // Write the object's name, position, rotation, and timestamp to the CSV file
+                outStream.WriteLine(objectName + "," + position.x + "," + position.y + "," + position.z + "," +
+                                    rotation.x + "," + rotation.y + "," + rotation.z + "," + timestamp);
+            }
+
+            // Wait for some time before tracking positions and rotations again
+            yield return new WaitForSeconds(1f); // Adjust this time interval as needed
+        }
+    }
+
+    // Function to create a new CSV file
+    void CreateNewFile()
+    {
+        // Open a new file at the specified path
+        outStream = System.IO.File.CreateText(filePath);
+    }
+
+    // Function to get the file path based on the platform
     private string getPath()
     {
         string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss"); // Get current timestamp
 #if UNITY_EDITOR
         return Application.dataPath + "/CSV/" + "Saved_data_" + timestamp + ".csv";
 #elif UNITY_ANDROID
-            return Application.persistentDataPath + "Saved_data_" + timestamp + ".csv";
+        return Application.persistentDataPath + "Saved_data_" + timestamp + ".csv";
 #elif UNITY_IPHONE
-            return Application.persistentDataPath + "/" + "Saved_data_" + timestamp + ".csv";
+        return Application.persistentDataPath + "/" + "Saved_data_" + timestamp + ".csv";
 #else
-            return Application.dataPath + "/" + "Saved_data_" + timestamp + ".csv";
+        return Application.dataPath + "/" + "Saved_data_" + timestamp + ".csv";
 #endif
     }
-} 
- */
+
+    // Function to handle closing the file stream when the application quits
+    void OnApplicationQuit()
+    {
+        // Close the file stream
+        if (outStream != null)
+        {
+            outStream.Close();
+        }
+    }
+}
+
--- Assets/scripts/eyeCursorController.cs
+++ Assets/scripts/eyeCursorController.cs
@@ -1,52 +1,9 @@
+
+/*
 using UnityEngine;
 using System.Collections;
 using System.IO;
 
-/*public class CursorTracker : MonoBehaviour
-{
-    private StreamWriter csvWriter;
-    private string csvFileName = "cursor_coordinates.csv";
-
-    void Start()
-    {
-        // Create a new CSV file or append to an existing one
-        csvWriter = new StreamWriter(Application.dataPath + "/" + csvFileName, true);
-        // Write headers if the file is just created
-        if (csvWriter.BaseStream.Length == 0)
-        {
-            csvWriter.WriteLine("Time, X, Y");
-        }
-    }
-
-    void Update()
-    {
-        // Get the position of the cursor
-        Vector3 cursorPos = Input.mousePosition;
-        // Convert screen coordinates to world coordinates if needed
-        // cursorPos = Camera.main.ScreenToWorldPoint(cursorPos); 
-
-        // Write the coordinates to the CSV file
-        string currentTime = Time.time.ToString();
-        string line = currentTime + "," + cursorPos.x.ToString() + "," + cursorPos.y.ToString();
-        csvWriter.WriteLine(line);
-    }
-
-    void OnDestroy()
-    {
-        // Close the CSV file when the script is destroyed
-        csvWriter.Close();
-    }
-}
-
-
-
-
-*/
-
-
-
-using UnityEngine;
-using System.IO;
 
 public class EyeCursorController : MonoBehaviour
 {
@@ -68,6 +25,7 @@ public class EyeCursorController : MonoBehaviour
         // Create/Open the CSV file for writing
         string filePath = Path.Combine(directoryPath, fileName);
         writer = new StreamWriter(filePath);
+        //record how wide and tall the screen is
         writer.WriteLine("3dX,3dY,3dZ,2dX.2dY");
 
         // Check if the main camera is assigned
@@ -86,9 +44,85 @@ public class EyeCursorController : MonoBehaviour
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(cursorPosition.x, cursorPosition.y, mainCamera.nearClipPlane));
 
         // Save data to CSV
-        writer.WriteLine($"{worldPosition.x},{worldPosition.y},{worldPosition.z}, {cursorPosition.x}, {cursorPosition.y}");
+        //if(writer!=null)
+            writer.WriteLine($"{worldPosition.x},{worldPosition.y},{worldPosition.z}, {cursorPosition.x}, {cursorPosition.y}");
         Debug.Log("2D: " + cursorPosition.ToString() + ", 3D: " +worldPosition.x.ToString()+" "+ worldPosition.y.ToString() + " " + worldPosition.z.ToString());
-        cursor.transform.position = worldPosition;
+        //cursor.transform.position = worldPosition;
+        // Check if the cursor GameObject is not null before accessing it
+        if (cursor != null)
+        {
+            cursor.transform.position = worldPosition;
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Close the StreamWriter when the script is destroyed
+        if (writer != null)
+        {
+            writer.Close();
+        }
+    }
+}
+
+
+
+*/
+
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class EyeCursorController : MonoBehaviour
+{
+    public Camera mainCamera; // Reference to the main camera
+    public string folderName = "EyeCursor";
+    public string fileName = "EyeGazeData.csv";
+    private StreamWriter writer;
+    public GameObject cursor;
+    public LayerMask raycastMask; // Specify the layers the raycast should hit
+
+    void Start()
+    {
+        // Create the directory if it doesn't exist
+        string directoryPath = Path.Combine(Application.dataPath, folderName);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        // Create/Open the CSV file for writing
+        string filePath = Path.Combine(directoryPath, fileName);
+        writer = new StreamWriter(filePath);
+        // Record how wide and tall the screen is
+        writer.WriteLine("3dX,3dY,3dZ,2dX.2dY");
+
+        // Check if the main camera is assigned
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    void Update()
+    {
+        // Cast a ray from the camera to determine the gaze direction
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, raycastMask))
+        {
+            // Save data to CSV
+            Vector3 worldPosition = hit.point;
+            Vector2 cursorPosition = Input.mousePosition;
+            writer.WriteLine($"{worldPosition.x},{worldPosition.y},{worldPosition.z}, {cursorPosition.x}, {cursorPosition.y}");
+
+            // Move the cursor to the hit point
+            if (cursor != null)
+            {
+                cursor.transform.position = worldPosition;
+            }
+        }
     }
 
     void OnDestroy()
